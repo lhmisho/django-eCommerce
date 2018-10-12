d4411be19665cb15b61cd9a912de23f2833260d5
lhmisho <lokman@divine-it.net>
2018-10-12 22:00:01 +0600

    Render HTML, added (TEMPLATE DIR TO TEMPLATES) rendered some html from home.html managing the url

--- eshop/eshop/views.py
+++ eshop/eshop/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1>hello world<h2>")
+    return render(request, 'home.html')
