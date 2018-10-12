bf7e0ab91dd4147f32f500508ec8674eefb7bd84
lhmisho <lokman@divine-it.net>
2018-10-13 00:45:55 +0600

    In this lesson discussed about django forms. also a simple validtion of email form, seen what is cleaned data etc

---
+++ eshop/eshop/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    fullname    = forms.CharField(label='Full name', max_length=120, widget=forms.TextInput(attrs={"class" : "form-control", "id": "fullname", "placeholder":"Full name"}))
+    email       = forms.EmailField(label='Email', max_length=120, widget=forms.EmailInput(attrs={"class" : "form-control", "id" : "contactId", "placeholder" : "Email"}))
+    content     = forms.CharField(label='Description', max_length=500, widget=forms.Textarea(attrs={"class" : "form-control", "id" : "contactDescription", "placeholder" : "Write something about your topic"}))
+
+    # a simple example of email validation
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'gmail.com' not in email:
+            raise forms.ValidationError("Email has to be GMAIL")
+        return email
--- eshop/eshop/views.py
+++ eshop/eshop/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# import ContactForm from .form
+from .forms import ContactForm
+
 def home_page(request):
     title = "I am from Home Page"
     content = "Welcome to the home page"
@@ -22,16 +25,22 @@ def about_page(request):
 def contact_page(request):
     title   = "I am from Contact Page"
     content = "Welcome to the contact page"
+
+    # created a instance of ContactForm()
+    contact_form = ContactForm(request.POST or None)   # if data is POST request else None
     context = {
         "title"     : title,
-        "content"   : content
+        "content"   : content,
+        "form"      : contact_form              # passing the form instance to context so that we can access it in view.html as {{form}}
     }
 
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
     # checking is this request is POST or not
-    if request.method == "POST":
-        print(request.POST)          # printing the requested value as dict
-        print(request.POST.get('fullname'))   # printing the acutal requested value
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    # if request.method == "POST":
+    #     print(request.POST)          # printing the requested value as dict
+    #     print(request.POST.get('fullname'))   # printing the acutal requested value
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
 
     return render(request, 'contact/view.html', context)
