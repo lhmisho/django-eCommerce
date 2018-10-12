cc78ae607f21616f6d6400d3cd6bc7db7b2d05c8
lhmisho <lokman@divine-it.net>
2018-10-12 23:14:09 +0600

    In this lesson, created a html form on 'contact/view.html'.Also checked that how POST method is working and printed the requested value which ginven by the form

--- eshop/eshop/views.py
+++ eshop/eshop/views.py
@@ -10,15 +10,6 @@ def home_page(request):
     }
     return render(request, 'home.html', context)
 
-def contact_page(request):
-    title   = "I am from Contact Page"
-    content = "Welcome to the contact page"
-    context = {
-        "title"     : title,
-        "content"   : content
-    }
-    return render(request, 'home.html', context)
-
 def about_page(request):
     title = "I am from About Page"
     content = "Welcome to the about page"
@@ -27,3 +18,20 @@ def about_page(request):
         "content"   : content
     }
     return render(request, 'home.html', context)
+
+def contact_page(request):
+    title   = "I am from Contact Page"
+    content = "Welcome to the contact page"
+    context = {
+        "title"     : title,
+        "content"   : content
+    }
+
+    # checking is this request is POST or not
+    if request.method == "POST":
+        print(request.POST)          # printing the requested value as dict
+        print(request.POST.get('fullname'))   # printing the acutal requested value
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+
+    return render(request, 'contact/view.html', context)
