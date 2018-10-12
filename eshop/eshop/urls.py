da0286735f2cf75cea110812bf87585f5bd2184f
lhmisho <lokman@divine-it.net>
2018-10-12 22:27:58 +0600

    This lesson discussed about Template Context

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import home_page, contact_page, about_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
 ]
--- eshop/eshop/views.py
+++ eshop/eshop/views.py
@@ -2,4 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, 'home.html')
+    title = "I am from Home Page"
+    content = "Welcome to the home page"
+    context = {
+        "title"     : title,
+        "content"   : content
+    }
+    return render(request, 'home.html', context)
+
+def contact_page(request):
+    title   = "I am from Contact Page"
+    content = "Welcome to the contact page"
+    context = {
+        "title"     : title,
+        "content"   : content
+    }
+    return render(request, 'home.html', context)
+
+def about_page(request):
+    title = "I am from About Page"
+    content = "Welcome to the about page"
+    context = {
+        "title"     : title,
+        "content"   : content
+    }
+    return render(request, 'home.html', context)
