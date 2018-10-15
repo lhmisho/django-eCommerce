2e7ba79aee8b611dc19a3cee6219b90b18ff2fd0
lhmisho <lokman@divine-it.net>
2018-10-15 18:25:17 +0600

    In this lesson we added product name as a title

--- eshop/products/models.py
+++ eshop/products/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     Price       = models.DecimalField(default=10.00, decimal_places=2, max_digits=19)
+
+    def __str__(self):
+        return self.title
