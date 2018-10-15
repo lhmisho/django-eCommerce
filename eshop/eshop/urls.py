fe9e1eadfefb88f763dfd4f1ee57dbd53e51aba9
lhmisho <lokman@divine-it.net>
2018-10-15 07:24:19 +0600

    In this lesson we covered Managing static files

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -26,3 +29,7 @@ urlpatterns = [
     path('register/', register_page),
     path('contact/', contact_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
