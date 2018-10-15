c6e0bf0dbc33b82057726be5cb38ea2e6e63fe20
lhmisho <lokman@divine-it.net>
2018-10-15 18:03:30 +0600

    In this lesson created our first Product Model

--- eshop/products/models.py
+++ eshop/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    Price       = models.DecimalField(default=10.00, decimal_places=2, max_digits=19)
---
+++ eshop/products/readme.md
@@ -0,0 +1,11 @@
+# underestending CRUD
+
+1) CREATE -- > 
+    -- > POST
+
+2) RETRIVE --> 
+    -- > LIST, SEARCH
+3) UPDATE -- > 
+    -- > PUT, PATCH, POST
+4) DELETE -- > 
+    -- > DELETE
