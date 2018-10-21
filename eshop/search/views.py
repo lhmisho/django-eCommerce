9e51f27b3efb59b4968ef8014950697f99576775
lhmisho <lokman@divine-it.net>
2018-10-21 13:16:06 +0600

    Display the query to the use

--- eshop/search/views.py
+++ eshop/search/views.py
@@ -6,7 +6,17 @@ from products.models import Product
 class SearchProductView(ListView):
     # first way to manage list view
     #queryset = Product.objects.all()
-    template_name = 'products/product_list.html'
+    template_name = 'search/view.html'
+
+    # passing request.GET.q as a query to the template
+    def get_context_data(self, *args, **kwargs):
+        request = self.request
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query   = request.GET.get('q')
+        context['query'] = query
+        return context
+
+
     def get_queryset(self, *args, **kwargs):
         request = self.request
         query = request.GET.get('q',None)
