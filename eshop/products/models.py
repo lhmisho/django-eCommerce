3fa43d63443a4631b014bb80c8acf1dfa2cfd6fb
lhmisho <lokman@divine-it.net>
2018-10-22 20:41:09 +0600

    Search by related model and the search component is completed

--- eshop/products/models.py
+++ eshop/products/models.py
@@ -33,7 +33,7 @@ class ProductQueryset(models.query.QuerySet):
         return self.filter(featured=True, active=True)
 
     def search(self, query):
-        lookups = Q(title__icontains = query) | Q(description__icontains = query) | Q(price__icontains = query)
+        lookups = Q(title__icontains = query) | Q(description__icontains = query) | Q(price__icontains = query) | Q(tag__title__icontains = query)
         return self.filter(lookups).distinct()
 # model manager for custom queryset
 class ProductManager(models.Manager):
