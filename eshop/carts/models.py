a7c9ed5a3a42c8f9a6d71be8c14643d38a0ad65f
lhmisho <lokman@divine-it.net>
2018-10-24 16:41:12 +0600

    Cart model manager part2

--- eshop/carts/models.py
+++ eshop/carts/models.py
@@ -6,6 +6,26 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
+
+    # creating cart object on model so that we can use it where ever we neeed
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+
+    # if the id is exits than go else create one
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            # if there is some issue than it's might me ( cart_obj = self.new_cart(user=request.user) )
+            cart_obj = self.new_cart(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+
+        return cart_id, new_obj
     def new_cart(self, user=None):
         print(user)
         user_obj = None
--- eshop/carts/views.py
+++ eshop/carts/views.py
@@ -10,17 +10,5 @@ def crete_cart(user=None):
     return cart_obj
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs = Cart.objects.filter(id=cart_id)
-
-    # if the id is exits than go else create one
-    if qs.count() == 1:
-        cart_obj = qs.first()
-        if request.user.is_authenticated and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new_cart(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-
+    cart_id = Cart.objects.new_or_get(request)
     return render(request, 'carts/cart_home.html', {})
