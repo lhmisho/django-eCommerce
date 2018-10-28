3efe508f7b62e06ea7206b550a8a676185df02ab
lhmisho <lokman@divine-it.net>
2018-10-28 20:38:22 +0600

    Add to cart form

--- eshop/carts/views.py
+++ eshop/carts/views.py
@@ -12,13 +12,24 @@ def cart_home(request):
 
 # Update view for cart
 def cart_update(request):
-    product_id  = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)    # we also added this line to products view on get_context_data so that we can add cart to specific product
+    print(request.POST)
+    # getting the product_id which is sent from the update_cart.html
+    product_id  = request.POST.get('product_id')
 
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
-    #return redirect(product_obj.get_absolute_url())
+    # if the product_id is not none then we taking it to product_obj
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except DoesNotExist:
+            print("product not exists")
+            return redirect("cart:home")   # if product is not exists simply just redirect to home
+        cart_obj, new_obj = Cart.objects.new_or_get(request)    # we also added this line to products view on get_context_data so that we can add cart to specific product
+
+        # if the product_obj is exists in cart than we can remove
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        # if the product_obj is exists in cart than we can add it
+        else:
+            cart_obj.products.add(product_obj)
+        #return redirect(product_obj.get_absolute_url())
     return redirect("cart:home")
