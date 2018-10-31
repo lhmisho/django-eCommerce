a69e7a271549c757781af9353a4bacf22bc74665
lhmisho <lokman@divine-it.net>
2018-10-31 21:19:09 +0600

    Calculate the order total

--- eshop/orders/models.py
+++ eshop/orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from eshop.utils import unique_order_id_generator
 from carts.models import Cart
 # Create your models here.
@@ -21,6 +21,13 @@ class Order(models.Model):
     shipping_total = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
     total   = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 
+    def update_total(self):
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total + shipping_total
+        self.total = new_total
+        self.save()
+        return new_total
 
     def __str__(self):
         return self.order_id
@@ -31,3 +38,28 @@ def pre_save_order_id(sender, instance, *args, **kewarg):
         instance.order_id = unique_order_id_generator(instance)
 
 pre_save.connect(pre_save_order_id , sender=Order)
+
+
+# using post save for cart total
+def post_save_cart_total(sender, instance, created, *args, **kewarg):
+    if not created:
+        cart_obj    = instance
+        cart_total  = cart_obj.total
+        cart_id     = cart_obj.id
+
+        qs = Order.objects.filter(cart__id=cart_id)
+
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    print("running")
+    if created:
+        print("updating")
+        instance.update_total()
+        print("running")
+
+post_save.connect(post_save_order, sender=Order)
